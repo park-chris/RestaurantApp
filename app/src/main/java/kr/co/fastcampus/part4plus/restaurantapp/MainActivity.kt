4b99823af29447cf76930af52e336e4da910aeac
박수정 <zxzx4342@naver.com>
2024-05-29 21:44:32 +0900

    Build Variants 적용

--- app/src/main/java/kr/co/fastcampus/part4plus/restaurantapp/MainActivity.kt
+++ app/src/main/java/kr/co/fastcampus/part4plus/restaurantapp/MainActivity.kt
@@ -3,11 +3,26 @@ package kr.co.fastcampus.part4plus.restaurantapp
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
+import kr.co.fastcampus.part4plus.restaurantapp.libraries.storage_contract.IStorage
+import kr.co.fastcampus.part4plus.restaurantapp.libraries.storage_contract.constants.StorageKeys
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var storage: IStorage
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        saveAppTheme()
+    }
+
+    private fun saveAppTheme() {
+        storage.save(
+            StorageKeys.APP_THEME,
+            BuildConfig.FLAVOR
+        )
     }
 }
--- core/src/main/java/kr/co/fastcampus/part4plus/restaurantapp/core/viewmodel/ThemeViewModel.kt
+++ core/src/main/java/kr/co/fastcampus/part4plus/restaurantapp/core/viewmodel/ThemeViewModel.kt
@@ -5,20 +5,39 @@ import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kr.co.fastcampus.part4plus.restaurantapp.libraries.storage_contract.IStorage
+import kr.co.fastcampus.part4plus.restaurantapp.libraries.storage_contract.constants.StorageKeys
 import kr.co.fastcampus.part4plus.restaurantapp.ui_components.config.ComponentConfig
 import kr.co.fastcampus.part4plus.restaurantapp.ui_components.config.DefaultComponentConfig
 import kr.co.fastcampus.part4plus.restaurantapp.ui_components.theme.color.ColorSet
 import javax.inject.Inject
 
 @HiltViewModel
-class ThemeViewModel @Inject constructor(): ViewModel() {
+class ThemeViewModel @Inject constructor(
+    private val storage: IStorage
+): ViewModel() {
+
+    private val isDefaultThemeRed = storage.get<String>(StorageKeys.APP_THEME) == "red"
+
     private val _themeState: MutableStateFlow<ComponentConfig> =
-        MutableStateFlow(DefaultComponentConfig.RED_THEME)
+        MutableStateFlow(
+            if (isDefaultThemeRed) {
+                DefaultComponentConfig.RED_THEME
+            } else {
+                DefaultComponentConfig.BLUE_THEME
+            }
+        )
     val themeState: StateFlow<ComponentConfig>
         get() = _themeState
 
     private val _nextColorState: MutableStateFlow<Color> =
-        MutableStateFlow(DefaultComponentConfig.BLUE_THEME.colors.LightColors.primary)
+        MutableStateFlow(
+            if (isDefaultThemeRed) {
+                DefaultComponentConfig.BLUE_THEME.colors.LightColors.primary
+            } else {
+                DefaultComponentConfig.BLUE_THEME.colors.LightColors.primary
+            }
+        )
     val nextColorState: StateFlow<Color>
         get() = _nextColorState
 
